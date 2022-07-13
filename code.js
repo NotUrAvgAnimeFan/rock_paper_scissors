4ec2efd8fa184ad7c5639d70aef954e539a63f14
NotUrAvgAnimeFan <rmorale5@jhu.edu>
2022-07-13 16:52:41 -0400

    completed functionality

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
+let compScore = 0;
+let playerScore = 0;
+
 function computerPlay() {
     let randomVal = Math.random() * 3;
 
@@ -17,22 +20,28 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection == "Rock") {
             return "Draw! Rock doesn't beat or lose to Rock";
         } else if (computerSelection == "Paper") {
-            return "You Lose! Paper beats Rock";
+            compScore++;
+            return "You Lose! Paper beats Rock"; 
         } else {
+            playerScore++;
             return "You Win! Rock beats Scissors";
         }
     } else if (playerSelection == "paper") {
         if (computerSelection == "Rock") {
+            playerScore++;
             return "You Win! Paper beats Rock";
         } else if (computerSelection == "Paper") {
             return "Draw! Paper doesn't beat or lose to Paper";
         } else {
+            compScore++;
             return "You Lose! Scissors beat Paper";
         }
     } else {
         if (computerSelection == "Rock") {
+            compScore++;
             return "You Lose! Rock beats Scissors";
         } else if (computerSelection == "Paper") {
+            playerScore++;
             return "You Win! Scissors beat Paper";
         } else {
             return "Draw! Scissors don't beat or lose to Scissors";
@@ -40,12 +49,42 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let userChoice;
-    let result;
-    for (let i = 0; i < 5; i++) {
-        userChoice = prompt("Choose your weapon!");
-        result = playRound(userChoice, computerPlay());
-        console.log(result);
+let results = document.querySelector('.results');
+let playerResults = document.querySelector('.player');
+let compResults = document.querySelector('.computer');
+
+
+let rock = document.querySelector('#rock');
+rock.addEventListener('click', ()=> {
+    results.textContent = playRound('rock', computerPlay());
+    playerResults.textContent = "Player Score: " + playerScore;
+    compResults.textContent = "Computer Score: " + compScore;
+});
+
+let paper = document.querySelector('#paper');
+paper.addEventListener('click', ()=> {
+    results.textContent = (playRound('paper', computerPlay()));
+    playerResults.textContent = "Player Score: " + playerScore;
+    compResults.textContent = "Computer Score: " + compScore;
+});
+
+let scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', ()=> {
+    results.textContent = (playRound('scissors', computerPlay()));
+    playerResults.textContent = "Player Score: " + playerScore;
+    compResults.textContent = "Computer Score: " + compScore;
+});
+
+let buttons = document.querySelector('.buttons');
+buttons.addEventListener('click', ()=> {
+    if (playerScore == 5) {
+        playerScore = 0;
+        compScore = 0;
+        alert("Player has won the game!");
+    } else if (compScore == 5){
+        playerScore = 0;
+        compScore = 0;
+        alert("Computer has won the game :(");
     }
-}
+});
+
